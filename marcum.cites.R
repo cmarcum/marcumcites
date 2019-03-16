9f8366eac8f9f5e416cc2f7c82345276eac2f6a0
Christopher Steven Marcum <cmarcum@uci.edu>
2019-03-16 17:18:58 -0400

    added all files from /MiscData/
    The marcum.cites.R file is the workhorse.

---
+++ marcum.cites.R
@@ -0,0 +1,47 @@
+#Generate an network based infographic from my publications
+#Date: 3/16/2019
+#Last updated: 3/16/2019
+#Author: Chris Marcum <cmarcum@uci.edu>
+
+library(network)
+library(sna)
+library(PNG)
+points.with.raster<-function(x,y,raster,width=10,height=10,...){
+  #x,y vectors are centroids
+  #raster is a list of raster arrays
+  x1<-x-(width/2)
+  x2<-x+(width/2)
+  y1<-y-(height/2)
+  y2<-y+(height/2)
+  points(x,y,...)
+  for(i in 1:length(x)){
+      rasterImage(as.raster(raster[[i]]),x1[i],y1[i],x2[i],y2[i])
+   }
+}
+
+#My bibtex files includes entries for "keywords" that are comma separated inset 
+#  networks, health, aging, methods, animals
+# They were imported using bib2df (which is really handy).
+
+if(0){
+ library(bib2df)
+ marcum<-bib2df("Dropbox/csm_cv/articlecv/marcum.bib")
+ keywords<-lapply(strsplit(marcum$KEYWORDS,split=","),function(x) gsub(" ","",x))
+ kw.sup<-na.omit(unique(unlist(keywords)))
+ for(i in 1:length(keywords)){
+  if(length(keywords[[i]])>0){
+   topics[keywords[[i]],keywords[[i]]]<-topics[keywords[[i]],keywords[[i]]]+1
+  }
+ }
+}
+
+load("marcum.cites.RData")
+icons<-sapply(dir(pattern="png"),readPNG)
+
+if(1){
+png("marcumtopics.png",width=800,height=800)
+par(mar=c(.5,.5,.5,.5))}
+gplot(topics,displaylabels=TRUE,usearrows=FALSE,edge.lwd=sqrt(topics),vertex.cex=log(diag(topics))+.05,vertex.col="cornflowerblue",vertex.border=FALSE,label.pos=3,coord=gcord,edge.col="gray",label.cex=1.25,jitter=FALSE)
+points.with.raster(gcord[,1],gcord[,2],icons[paste(colnames(topics),"png",sep=".")],width= ifelse(prop.table(diag(topics))<.1,.1,prop.table(diag(topics))),height= ifelse(prop.table(diag(topics))<.1,.1,prop.table(diag(topics))),cex=0)
+dev.off()
+
