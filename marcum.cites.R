fab1a607bd57dc7da2e3423d4f8bc3ab745522c8
cmarcum <cmarcum@uci.edu>
2019-12-25 07:47:10 -0500

    Updated CV

--- marcum.cites.R
+++ marcum.cites.R
@@ -1,6 +1,6 @@
 #Generate an network based infographic from my publications
 #Date: 3/16/2019
-#Last updated: 3/16/2019
+#Last updated: 12/25/2019
 #Author: Chris Marcum <cmarcum@uci.edu>
 
 library(network)
@@ -25,9 +25,10 @@ points.with.raster<-function(x,y,raster,width=10,height=10,...){
 
 if(0){
  library(bib2df)
- marcum<-bib2df("https://www.dropbox.com/s/k15vwyr5fe2itdk/marcum.bib?dl=1")
+ marcum<-bib2df("https://www.dropbox.com/s/pmk7aend6z5tqyv/marcum.bib?dl=1")
  keywords<-lapply(strsplit(marcum$KEYWORDS,split=","),function(x) gsub(" ","",x))
  kw.sup<-na.omit(unique(unlist(keywords)))
+ topics<-matrix(0,nrow=length(kw.sup),ncol=length(kw.sup),dimnames=list(kw.sup,kw.sup))
  for(i in 1:length(keywords)){
   if(length(keywords[[i]])>0){
    topics[keywords[[i]],keywords[[i]]]<-topics[keywords[[i]],keywords[[i]]]+1
@@ -36,12 +37,14 @@ if(0){
 }
 
 load("marcum.cites.RData")
-icons<-sapply(dir(pattern="png"),readPNG)
+#Read in all the icons
+#icons<-sapply(dir(pattern="png"),readPNG)
+#icons["marcumtopics.png"]<-NULL
 
-if(1){
-png("marcumtopics.png",width=800,height=800,res=100)
-par(mar=c(.5,.5,.5,.5))}
-gplot(topics,displaylabels=TRUE,usearrows=FALSE,edge.lwd=sqrt(topics),vertex.cex=log(diag(topics))+.05,vertex.col="cornflowerblue",vertex.border=FALSE,label.pos=3,coord=gcord,edge.col="gray",label.cex=1.25,jitter=FALSE)
-points.with.raster(gcord[,1],gcord[,2],icons[paste(colnames(topics),"png",sep=".")],width= ifelse(prop.table(diag(topics))<.1,.1,prop.table(diag(topics))),height= ifelse(prop.table(diag(topics))<.1,.1,prop.table(diag(topics))),cex=0)
+        if(0{
+        png("marcumtopics.png",width=800,height=800,res=100)
+        par(mar=c(.5,.5,.5,.5))}
+        gplot(topics,displaylabels=TRUE,usearrows=FALSE,edge.lwd=sqrt(topics),vertex.cex=log(diag(topics))+.05,vertex.col="cornflowerblue",vertex.border=FALSE,label.pos=3,coord=gcord,edge.col="gray",label.cex=1.25,jitter=FALSE)
+        points.with.raster(gcord[,1],gcord[,2],icons[paste(colnames(topics),"png",sep=".")],width= ifelse(prop.table(diag(topics))<.1,.1,prop.table(diag(topics))),height= ifelse(prop.table(diag(topics))<.1,.1,prop.table(diag(topics))),cex=0)
 dev.off()
 
